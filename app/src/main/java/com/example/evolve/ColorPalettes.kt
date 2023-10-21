6f01c08d32f4b4057b1a06a6ef7acb4551cb79a7
Bianca Calderón <98110978+BiancaCalderon@users.noreply.github.com>
2023-10-21 16:13:14 -0600

    Main Screen additions

---
+++ app/src/main/java/com/example/evolve/ColorPalettes.kt
@@ -0,0 +1,40 @@
+package com.example.evolve
+
+import androidx.compose.material.Colors
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+
+// ColorPalettes.kt
+
+
+val LightColorPalette = Colors(
+    primary = Color(0xFF6200EE),
+    primaryVariant = Color(0xFF3700B3),
+    secondary = Color(0xFF03DAC6),
+    secondaryVariant = Color(0xFF018786),
+    background = Color(0xFFFFFFFF),
+    surface = Color(0xFFFFFFFF),
+    error = Color(0xFFB00020),
+    onPrimary = Color(0xFFFFFFFF),
+    onSecondary = Color(0xFF000000),
+    onBackground = Color(0xFF000000),
+    onSurface = Color(0xFF000000),
+    onError = Color(0xFFFFFFFF),
+    isLight = true // Agrega el parámetro isLight con el valor correspondiente
+)
+
+val DarkColorPalette = Colors(
+    primary = Color(0xFFBB86FC),
+    primaryVariant = Color(0xFF3700B3),
+    secondary = Color(0xFF03DAC6),
+    secondaryVariant = Color(0xFF018786),
+    background = Color(0xFF121212),
+    surface = Color(0xFF121212),
+    error = Color(0xFFCF6679),
+    onPrimary = Color(0xFF000000),
+    onSecondary = Color(0xFF000000),
+    onBackground = Color(0xFFFFFFFF),
+    onSurface = Color(0xFFFFFFFF),
+    onError = Color(0xFF000000),
+    isLight = false // Agrega el parámetro isLight con el valor correspondiente
+)
--- app/src/main/java/com/example/evolve/MainActivity.kt
+++ app/src/main/java/com/example/evolve/MainActivity.kt
@@ -9,21 +9,32 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -38,6 +49,7 @@ import com.example.evolve.Model.PersonApp
 import com.example.evolve.Navigation.Navigation
 import com.example.evolve.ui.theme.EvolveTheme
 import kotlinx.coroutines.launch
+
 
 class MainActivity : ComponentActivity() {
 
@@ -67,66 +79,72 @@ class MainActivity : ComponentActivity() {
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun WelcomeScreen(navController: NavController,userName: String) {
-
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small)),
-        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_small))
+fun WelcomeScreen(navController: NavController, userName: String) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        item(span = { GridItemSpan(maxLineSpan) }) {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color(android.graphics.Color.parseColor("#f2edf6")))
-                    .padding(16.dp)
-            ) {
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = "EVOLVE",
-                        fontWeight = FontWeight.Bold,
-                        modifier = Modifier.padding(32.dp)
-                    )
-                    Image(
-                        painter = painterResource(id = R.drawable.evolve),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .size(32.dp)
-                            .padding(8.dp)
-                    )
-                }
-            }
-        }
         item {
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .border(1.dp, Color.White)
-                    .height(80.dp)
+                    .background(Color(0xFF000000))
+                    .padding(16.dp)
             ) {
-                Text(
-                    "Bienvenido $userName",
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.wrapContentSize(Alignment.Center)
+                Image(
+                    painter = painterResource(id = R.drawable.evolve),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .size(256.dp)
+                        .padding(8.dp)
                 )
             }
         }
         item {
+            Text(
+                text = "Bienvenido, $userName",
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+        }
+        item {
+            Spacer(modifier = Modifier.height(64.dp)) // Espacio entre la imagen y el botón
             Button(
                 onClick = { /*...*/ },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(dimensionResource(R.dimen.padding_small))
+                    .background(
+                        color = Color(0xFF000000), // Cambia el color de fondo del botón
+                    )
             ) {
-                Text("Ejercicios")
+                Text("Empezemos!", color = Color.White)
+            }
+
+
+
+        }
+        item {
+            Spacer(modifier = Modifier.height(32.dp)) // Espacio entre el botón y la parte inferior
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                IconButton(
+                    onClick = {
+                        // Tu lógica para cambiar el modo claro/oscuro
+                    }
+                ) {
+
+                }
             }
         }
     }
 }
+
+
 
 
 @Preview(showBackground = true)
