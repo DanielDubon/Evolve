02ab0f3535d958a3db00f15c40fab17064668666
DanielDubon <dub22233@uvg.edu.gt>
2023-10-21 21:00:08 -0600

    FrontEnd Login/Register Done

--- app/src/main/java/com/example/evolve/View/LoginScreen.kt
+++ app/src/main/java/com/example/evolve/View/LoginScreen.kt
@@ -1,14 +1,23 @@
 package com.example.evolve.View
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.SnackbarDefaults.backgroundColor
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -19,13 +28,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.evolve.Model.PersonApp
+import com.example.evolve.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -38,13 +54,47 @@ fun LoginScreen(app: PersonApp,navcontroller: NavController ) {
     val username = remember { mutableStateOf(TextFieldValue()) }
     val password = remember { mutableStateOf(TextFieldValue()) }
 
+    val logoColor = colorResource(id = R.color.LogoColor)
+
+    val brush = Brush.linearGradient(
+        colors = listOf(
+            logoColor,
+            Color.Black
+        ),
+
+    )
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .background(brush)
+            .offset(y = 140.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+
     ) {
+        val LogoPainter = painterResource(id = R.drawable.evolve)
+
+        Box(
+            contentAlignment = Alignment.Center ,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(10.dp)
+
+
+
+        ) {
+            Image(
+                painter = LogoPainter,
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(150.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+
+        }
+        Spacer(modifier = Modifier.height(10.dp))
         TextField(
             value = username.value,
             onValueChange = { username.value = it },
@@ -64,7 +114,9 @@ fun LoginScreen(app: PersonApp,navcontroller: NavController ) {
 
 Row {
     val coroutineScope2 = rememberCoroutineScope()
-    Button(onClick = {
+    Button(
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Black, contentColor = logoColor )
+        , onClick = {
 
         coroutineScope2.launch(Dispatchers.IO) {  // Use Dispatchers.IO for database operations
             if (app.room.personDao().searchUser(username.value.text, password.value.text)){
@@ -98,7 +150,10 @@ Row {
     }) {
         Text("Ingresar")
     }
-    Button(onClick = {
+
+    Spacer(modifier = Modifier.width(10.dp))
+
+    Button(colors = ButtonDefaults.buttonColors(containerColor = Color.Black, contentColor = logoColor ), onClick = {
         navcontroller.navigate("Register")
     }) {
         Text("Registrarse")
--- app/src/main/java/com/example/evolve/View/RegisterScreen.kt
+++ app/src/main/java/com/example/evolve/View/RegisterScreen.kt
@@ -2,14 +2,21 @@ package com.example.evolve.View
 
 import android.util.Log
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SnackbarHostState
@@ -22,7 +29,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,6 +43,7 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.evolve.Model.Person
 import com.example.evolve.Model.PersonApp
+import com.example.evolve.R
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.Dispatchers
 
@@ -46,14 +60,45 @@ fun RegisterScreen(app: PersonApp, navcontroller: NavController){
     val height = remember { mutableStateOf(TextFieldValue()) }
     val password = remember { mutableStateOf(TextFieldValue()) }
     val password2 = remember { mutableStateOf(TextFieldValue()) }
+    val LogoPainter = painterResource(id = R.drawable.evolve)
+    val logoColor = colorResource(id = R.color.LogoColor)
+    val brush = Brush.linearGradient(
+        colors = listOf(
+            logoColor,
+            Color.Black
+        ),
 
+        )
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .background(brush),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+
+        Box(
+            contentAlignment = Alignment.Center ,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(10.dp)
+
+
+
+        ) {
+            Image(
+                painter = LogoPainter,
+                contentDescription = "Logo",
+                modifier = Modifier
+                    .width(150.dp)
+                    .height(150.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+
+        }
+        Spacer(modifier = Modifier.height(10.dp))
         TextField(
             value = username.value,
             onValueChange = { username.value = it },
@@ -73,7 +118,7 @@ fun RegisterScreen(app: PersonApp, navcontroller: NavController){
         TextField(
             value = height.value,
             onValueChange = { height.value = it },
-            label = { Text("Altura(m)") },
+            label = { Text("Altura(cm)") },
             singleLine = true
         )
         Spacer(modifier = Modifier.height(10.dp))
@@ -109,41 +154,68 @@ fun RegisterScreen(app: PersonApp, navcontroller: NavController){
 
             val coroutineScope = rememberCoroutineScope()
             Button(
-
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Black, contentColor = logoColor),
                 onClick = {
-                    if (password.value.text == password2.value.text) {
-                        val nombre = username.value.text
-                        val edad = age.value.text.toInt()
-                        val peso = weight.value.text.toInt()
-                        val altura = height.value.text.toInt()
-                        val contraseña = password.value.text
+                    try {
 
 
-                        coroutineScope.launch(Dispatchers.IO) {  // Use Dispatchers.IO for database operations
-                            val person = Person(id = 1, name = nombre, Height = altura, age = edad, weight = peso, password = contraseña)
-                            app.room.personDao().insertUser(listOf(person))
+                        if (password.value.text != "" && username.value.text != "" && age.value.text != "" && weight.value.text != "" && height.value.text != "") {
+                            if (password.value.text == password2.value.text) {
+                                val nombre = username.value.text
+                                val edad = age.value.text.toInt()
+                                val peso = weight.value.text.toInt()
+                                val altura = height.value.text.toInt()
+                                val contraseña = password.value.text
 
-                            val people = app.room.personDao().getAll()
-                            Log.d("BASEDEDATOSXXX", "HAY: ${people.size} UWU")
 
-                            // Switch back to the main thread to interact with the UI
-                            launch(Dispatchers.Main) {
-                                navcontroller.navigate("Login")
+                                coroutineScope.launch(Dispatchers.IO) {  // Use Dispatchers.IO for database operations
+                                    val person = Person(
+                                        id = 1,
+                                        name = nombre,
+                                        Height = altura,
+                                        age = edad,
+                                        weight = peso,
+                                        password = contraseña
+                                    )
+                                    app.room.personDao().insertUser(listOf(person))
+
+                                    val people = app.room.personDao().getAll()
+                                    Log.d("BASEDEDATOSXXX", "HAY: ${people.size} UWU")
+
+                                    // Switch back to the main thread to interact with the UI
+                                    launch(Dispatchers.Main) {
+                                        navcontroller.navigate("Login")
+                                    }
+                                }
+                            } else {
+                                Toast.makeText(
+                                    context,
+                                    "LAS CONTRASEÑAS NO SON IGUALES",
+                                    Toast.LENGTH_SHORT
+                                ).show()
                             }
+
+                        } else {
+                            Toast.makeText(
+                                context,
+                                "Completa todos los campos",
+                                Toast.LENGTH_SHORT
+                            ).show()
                         }
-                    } else {
+                    }catch (e: Exception){
                         Toast.makeText(
                             context,
-                            "LAS CONTRASEÑAS NO SON IGUALES",
+                            "Verifica los datos de los campos",
                             Toast.LENGTH_SHORT
                         ).show()
                     }
-
-
                 }) {
                 Text("Registrarse")
             }
-            Button(onClick = {
+            Spacer(modifier = Modifier.width(10.dp))
+            Button(
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Black, contentColor = logoColor ),
+                onClick = {
                 navcontroller.navigate("Login")
             }) {
                 Text("¿Ya tienes cuenta?")
